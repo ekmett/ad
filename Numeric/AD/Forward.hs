38ab17e564c0d6a5346bea267afe62b1de29a481
Edward Kmett <ekmett@gmail.com>
2010-06-05 18:39:30 +0100

    merged barak's lazier forward bind/bind2 and moved to more consistent indentation
    Ignore-this: dd977fc9438b79323352713d81047ee0
    
    darcs-hash:20100605173930-7b3c0-dcbaf52a557c69c13504c62f221df03b36306c71.gz

--- Numeric/AD/Forward.hs
+++ Numeric/AD/Forward.hs
@@ -79,21 +79,26 @@ instance Lifted Forward => Jacobian Forward where
     type D Forward = Id
     unary f (Id dadb) (Forward b db) = Forward (f b) (dadb * db)
     lift1 f df (Forward b db) = Forward (f b) (dadb * db)
-        where Id dadb = df (Id b)
+        where 
+            Id dadb = df (Id b)
     lift1_ f df (Forward b db) = Forward a da
-        where a = f b
-              Id da = df (Id a) (Id b) ^* db
+        where 
+            a = f b
+            Id da = df (Id a) (Id b) ^* db
 
     binary f (Id dadb) (Id dadc) (Forward b db) (Forward c dc) = Forward (f b c) da
-        where da = dadb * db + dc * dadc
+        where 
+            da = dadb * db + dc * dadc
     lift2 f df (Forward b db) (Forward c dc) = Forward a da
-        where a = f b c
-              (Id dadb, Id dadc) = df (Id b) (Id c) 
-              da = dadb * db + dc * dadc
+        where 
+            a = f b c
+            (Id dadb, Id dadc) = df (Id b) (Id c) 
+            da = dadb * db + dc * dadc
     lift2_ f df (Forward b db) (Forward c dc) = Forward a da
-        where a = f b c
-              (Id dadb, Id dadc) = df (Id a) (Id b) (Id c)
-              da = dadb * db + dc * dadc
+        where 
+            a = f b c
+            (Id dadb, Id dadc) = df (Id a) (Id b) (Id c)
+            da = dadb * db + dc * dadc
 
 deriveLifted $ conT ''Forward
 
@@ -128,25 +133,26 @@ diff2UF f a = unbundle <$> apply f a
 {-# INLINE diff2UF #-}
 
 bind :: (Traversable f, Num a) => (f (AD Forward a) -> b) -> f a -> f b
-bind f as = snd $ mapAccumL outer 0 as
+bind f as = snd $ mapAccumL outer 0
     where
         outer !i a = (i + 1, f $ snd $ mapAccumL (inner i) 0 as)
-        inner !i !j a = (j + 1, if i == j then bundle a 1 else lift a)
+        inner !i !j a = (j + 1, bundle a $ if i == j then 1 else 0)
 
 bind2 :: (Traversable f, Num a) => (f (AD Forward a) -> b) -> f a -> (b, f b)
 bind2 f as = dropIx $ mapAccumL outer (0, b0) as 
     where
         outer (!i, _) a = let b = f $ snd $ mapAccumL (inner i) 0 as in ((i + 1, b), b)
-        inner !i !j a = (j + 1, if i == j then bundle a 1 else lift a)
+        inner !i !j a = (j + 1, bundle a $ if i == j then 1 else 0)
         b0 = f (lift <$> as)
         dropIx ((_,b),bs) = (b,bs)
 
 -- we can't transpose arbitrary traversables, since we can't construct one out of whole cloth, and the outer
 -- traversable could be empty. So instead we use one as a 'skeleton'
 transposeWith :: (Functor f, Foldable f, Traversable g) => (b -> f a -> c) -> f (g a) -> g b -> g c
-transposeWith f as bs = snd $ mapAccumL go xss0 bs 
-    where go xss b = (tail <$> xss, f b (head <$> xss))
-          xss0 = toList <$> as
+transposeWith f as = snd . mapAccumL go xss0
+    where 
+        go xss b = (tail <$> xss, f b (head <$> xss))
+        xss0 = toList <$> as
 
 -- A fast, simple transposed forward jacobian
 jacobianT :: (Traversable f, Functor g, Num a) => (forall s. Mode s => f (AD s a) -> g (AD s a)) -> f a -> f (g a)
@@ -156,13 +162,15 @@ jacobianT f = bind (fmap tangent . f)
 
 jacobian :: (Traversable f, Traversable g, Num a) => (forall s. Mode s => f (AD s a) -> g (AD s a)) -> f a -> g (f a)
 jacobian f as = transposeWith (const id) t p
-    where (p, t) = bind2 (fmap tangent . f) as 
+    where 
+        (p, t) = bind2 (fmap tangent . f) as 
 {-# INLINE jacobian #-}
 
 jacobian2 :: (Traversable f, Traversable g, Num a) => (forall s. Mode s => f (AD s a) -> g (AD s a)) -> f a -> g (a, f a)
 jacobian2 f as = transposeWith row t p
-    where (p, t) = bind2 f as 
-          row x as = (primal x, tangent <$> as) 
+    where 
+        (p, t) = bind2 f as 
+        row x as = (primal x, tangent <$> as) 
 {-# INLINE jacobian2 #-}
 
 grad :: (Traversable f, Num a) => (forall s. Mode s => f (AD s a) -> AD s a) -> f a -> f a
@@ -171,7 +179,8 @@ grad f = bind (tangent . f)
 
 grad2 :: (Traversable f, Num a) => (forall s. Mode s => f (AD s a) -> AD s a) -> f a -> (a, f a)
 grad2 f as = (primal b, tangent <$> bs)
-    where (b, bs) = bind2 f as
+    where 
+        (b, bs) = bind2 f as
 {-# INLINE grad2 #-}
 
 

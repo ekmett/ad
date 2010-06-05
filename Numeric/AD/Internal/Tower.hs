2f3c28599f5d04a0099e7715979c34b6d39d6f6c
Edward Kmett <ekmett@gmail.com>
2010-06-06 00:08:18 +0100

    added missing internal modules
    Ignore-this: 5e5bde95987008a122a91d70e643e97
    
    darcs-hash:20100605230818-7b3c0-e57a51b175d3eb963d802e008272b07943704163.gz

---
+++ Numeric/AD/Internal/Tower.hs
@@ -0,0 +1,103 @@
+{-# LANGUAGE Rank2Types, TypeFamilies, FlexibleContexts, UndecidableInstances, TemplateHaskell #-}
+-----------------------------------------------------------------------------
+-- |
+-- Module      : Numeric.AD.Tower.Internal
+-- Copyright   : (c) Edward Kmett 2010
+-- License     : BSD3
+-- Maintainer  : ekmett@gmail.com
+-- Stability   : experimental
+-- Portability : GHC only 
+--
+-----------------------------------------------------------------------------
+
+module Numeric.AD.Internal.Tower
+    ( Tower(..)
+    , zeroPad
+    , d
+    , d2
+    , tangents
+    , bundle
+    , apply
+    , getADTower
+    ) where
+
+import Control.Applicative
+import Numeric.AD.Classes
+import Numeric.AD.Internal
+import Language.Haskell.TH
+import Data.List (mapAccumL)
+
+-- | @Tower@ is an AD 'Mode' that calculates a tangent tower by forward AD, and provides fast 'diffsUU', 'diffsUF'
+newtype Tower a = Tower { getTower :: [a] } deriving (Show)
+
+-- Local combinators
+
+zeroPad :: Num a => [a] -> [a]
+zeroPad xs = xs ++ repeat 0 
+{-# INLINE zeroPad #-}
+
+d :: Num a => [a] -> a
+d (_:da:_) = da
+d _ = 0
+{-# INLINE d #-}
+
+d2 :: Num a => [a] -> (a, a) 
+d2 (a:da:_) = (a, da)
+d2 (a:_)    = (a, 0)
+d2 _        = (0, 0)
+{-# INLINE d2 #-}
+
+tangents :: Tower a -> Tower a
+tangents (Tower []) = Tower []
+tangents (Tower (_:xs)) = Tower xs
+{-# INLINE tangents #-}
+
+bundle :: a -> Tower a -> Tower a
+bundle a (Tower as) = Tower (a:as)
+{-# INLINE bundle #-}
+
+apply :: Num a => (AD Tower a -> b) -> a -> b
+apply f a = f (AD (Tower [a,1]))
+{-# INLINE apply #-}
+
+getADTower :: AD Tower a -> [a]
+getADTower (AD t) = getTower t
+{-# INLINE getADTower #-}
+
+instance Primal Tower where
+    primal (Tower (x:_)) = x
+    primal _ = 0
+
+instance Lifted Tower => Mode Tower where
+    lift a = Tower [a]
+    zero = Tower []
+
+    Tower [] <+> bs = bs
+    as <+> Tower [] = as
+    Tower (a:as) <+> Tower (b:bs) = Tower (c:cs)
+        where 
+            c = a + b
+            Tower cs = Tower as <+> Tower bs
+
+    a *^ Tower bs = Tower (map (a*) bs)
+    Tower as ^* b = Tower (map (*b) as)
+
+    Tower as ^/ b = Tower (map (/b) as)
+
+instance Lifted Tower => Jacobian Tower where
+    type D Tower = Tower
+    unary f dadb b = bundle (f (primal b)) (tangents b *! dadb)
+    lift1 f df b   = bundle (f (primal b)) (tangents b *! df b)
+    lift1_ f df b = a where 
+        a = bundle (f (primal b)) (tangents b *! df a b)
+
+    binary f dadb dadc b c = bundle (f (primal b) (primal c)) (tangents b *! dadb +! tangents c *! dadc) 
+    lift2 f df b c = bundle (f (primal b) (primal c)) (tangents b *! dadb +! tangents c *! dadc) where
+        (dadb, dadc) = df b c 
+    lift2_ f df b c = a where 
+        a0 = f (primal b) (primal c)
+        da = tangents b *! dadb +! tangents c *! dadc
+        a = bundle a0 da 
+        (dadb, dadc) = df a b c
+
+deriveLifted (conT ''Tower)
---
+++ Numeric/AD/Newton.hs
@@ -0,0 +1,68 @@
+{-# LANGUAGE Rank2Types, BangPatterns #-}
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Numeric.AD.Newton
+-- Copyright   :  (c) Edward Kmett 2010
+-- License     :  BSD3
+-- Maintainer  :  ekmett@gmail.com
+-- Stability   :  experimental
+-- Portability :  GHC only 
+--
+-----------------------------------------------------------------------------
+
+module Numeric.AD.Newton
+    ( 
+    -- * Newton's Method (Forward AD)
+      findZero
+    , inverse
+    , fixedPoint
+    , extremum
+    -- * Gradient Descent (Reverse AD)
+    , gradientDescent
+    -- * Exposed Types
+    , AD(..)
+    , Mode(..)
+    ) where
+
+import Prelude hiding (all)
+import Numeric.AD.Classes
+import Numeric.AD.Internal
+import Data.Foldable (all)
+import Data.Traversable (Traversable)
+import qualified Numeric.AD as M
+import Data.Ix
+import Numeric.AD.Forward (diff, diff2) 
+import Numeric.AD.Reverse (grad2) 
+
+findZero :: Fractional a => (forall s. Mode s => AD s a -> AD s a) -> a -> [a]
+findZero f x0 = iterate (\x -> let (y,y') = diff2 f x in x - y/y') x0
+{-# INLINE findZero #-}
+
+inverse :: Fractional a => (forall s. Mode s => AD s a -> AD s a) -> a -> a -> [a]
+inverse f x0 y = findZero (\x -> f x - lift y) x0
+{-# INLINE inverse  #-}
+
+fixedPoint :: Fractional a => (forall s. Mode s => AD s a -> AD s a) -> a -> [a]
+fixedPoint f = findZero (\x -> f x - x)
+{-# INLINE fixedPoint #-}
+
+extremum :: Fractional a => (forall t s. (Mode t, Mode s) => AD t (AD s a) -> AD t (AD s a)) -> a -> [a]
+extremum f x0 = findZero (diff f) x0
+{-# INLINE extremum #-}
+
+gradientDescent :: (Traversable f, Fractional a, Ord a) => (forall s. Mode s => f (AD s a) -> AD s a) -> f a -> [f a]
+gradientDescent f x0 = go x0 fx0 gx0 0.1 0
+    where
+        (fx0, gx0) = grad2 f x0
+        go x fx gx !eta !i 
+            | eta == 0     = [] -- step size is 0
+            | fx1 > fx     = go x fx gx (eta/2) 0 -- we stepped too far
+            | all (==0) gx = [] -- gradient is 0
+            | otherwise    = x1 : if i == 10
+                                  then go x1 fx1 gx1 (eta*2) 0
+                                  else go x1 fx1 gx1 eta (i+1)
+            where
+                -- should check gx = 0 here
+                x1 = zipWithT (\xi gxi -> xi - eta * gxi) x gx
+                (fx1, gx1) = grad2 f x1
+{-# INLINE gradientDescent #-}

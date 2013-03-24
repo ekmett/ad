fb606e88e7f7bc4ec37253a026e23da74f138865
Alex Lang <me@alang.ca>
2013-03-24 15:17:16 +0900

    Comment out the old deriveNumeric

--- src/Numeric/AD/Internal/Classes.hs
+++ src/Numeric/AD/Internal/Classes.hs
@@ -20,7 +20,7 @@ module Numeric.AD.Internal.Classes
     -- * Automatically Deriving AD
     , Jacobian(..)
     , Primal(..)
-    , deriveNumeric
+    -- , deriveNumeric
     , deriveNumeric'
     , Iso(..)
     , Scalar
@@ -149,100 +149,100 @@ discrete3 f x y z = f (primal x) (primal y) (primal z)
 -- > instance ('RealFloat' a) => 'RealFloat' ($g a)
 -- > instance ('RealFrac' a) => 'RealFrac' ($g a)
 -- > instance ('Real' a) => 'Real' ($g a)
-deriveNumeric :: ([Pred] -> [Pred]) -> Type -> Q [Dec]
-deriveNumeric f tCon = map fudgeCxt <$> lifted
-    where
-      t = pure tCon
-      fudgeCxt (InstanceD cxt typ dec) = InstanceD (f cxt) typ dec
-      fudgeCxt _ = error "Numeric.AD.Internal.Classes.deriveNumeric_fudgeCxt: Not InstanceD"
-      lifted = [d|
-       instance (Num a, Eq a) => Eq ($t a) where
-        (==)          = discrete2 (==)
-       instance (Num a, Ord a) => Ord ($t a) where
-        compare       = discrete2 compare
-       instance (Num a, Bounded a) => Bounded ($t a) where
-        maxBound      = auto maxBound
-        minBound      = auto minBound
-       instance Num a => Num ($t a) where
-        fromInteger 0  = zero
-        fromInteger n = auto (fromInteger n)
-        (+)          = (<+>) -- binary (+) one one
-        (-)          = binary (-) (auto 1) (auto (-1)) -- TODO: <-> ? as it is, this might be pretty bad for Tower
-        (*)          = lift2 (*) (\x y -> (y, x))
-        negate       = lift1 negate (const (auto (-1)))
-        abs          = lift1 abs signum
-        signum a     = lift1 signum (const zero) a
-       instance Fractional a => Fractional ($t a) where
-        fromRational 0 = zero
-        fromRational r = auto (fromRational r)
-        x / y        = x * recip y
-        recip        = lift1_ recip (const . negate . join (*))
-       instance Floating a => Floating ($t a) where
-        pi       = auto pi
-        exp      = lift1_ exp const
-        log      = lift1 log recip
-        logBase x y = log y / log x
-        sqrt     = lift1_ sqrt (\z _ -> recip (auto 2 * z))
-        (**)     = (<**>)
-        --x ** y
-        --   | isKnownZero y     = 1
-        --   | isKnownConstant y, y' <- primal y = lift1 (** y') ((y'*) . (**(y'-1))) x
-        --   | otherwise         = lift2_ (**) (\z xi yi -> (yi * z / xi, z * log1 xi)) x y
-        sin      = lift1 sin cos
-        cos      = lift1 cos $ negate . sin
-        tan      = lift1 tan $ recip . join (*) . cos
-        asin     = lift1 asin $ \x -> recip (sqrt (auto 1 - join (*) x))
-        acos     = lift1 acos $ \x -> negate (recip (sqrt (one - join (*) x)))
-        atan     = lift1 atan $ \x -> recip (one + join (*) x)
-        sinh     = lift1 sinh cosh
-        cosh     = lift1 cosh sinh
-        tanh     = lift1 tanh $ recip . join (*) . cosh
-        asinh    = lift1 asinh $ \x -> recip (sqrt (one + join (*) x))
-        acosh    = lift1 acosh $ \x -> recip (sqrt (join (*) x - one))
-        atanh    = lift1 atanh $ \x -> recip (one - join (*) x)
-       instance (Num a, Enum a) => Enum ($t a) where
-        succ                 = lift1 succ (const one)
-        pred                 = lift1 pred (const one)
-        toEnum               = auto . toEnum
-        fromEnum             = discrete1 fromEnum
-        enumFrom a           = withPrimal a <$> enumFrom (primal a)
-        enumFromTo a b       = withPrimal a <$> discrete2 enumFromTo a b
-        enumFromThen a b     = zipWith (fromBy a delta) [0..] $ discrete2 enumFromThen a b where delta = b - a
-        enumFromThenTo a b c = zipWith (fromBy a delta) [0..] $ discrete3 enumFromThenTo a b c where delta = b - a
-       instance Real a => Real ($t a) where
-        toRational      = discrete1 toRational
-       instance RealFloat a => RealFloat ($t a) where
-        floatRadix      = discrete1 floatRadix
-        floatDigits     = discrete1 floatDigits
-        floatRange      = discrete1 floatRange
-        decodeFloat     = discrete1 decodeFloat
-        encodeFloat m e = auto (encodeFloat m e)
-        isNaN           = discrete1 isNaN
-        isInfinite      = discrete1 isInfinite
-        isDenormalized  = discrete1 isDenormalized
-        isNegativeZero  = discrete1 isNegativeZero
-        isIEEE          = discrete1 isIEEE
-        exponent = exponent
-        scaleFloat n = unary (scaleFloat n) (scaleFloat n one)
-        significand x =  unary significand (scaleFloat (- floatDigits x) one) x
-        atan2 = lift2 atan2 $ \vx vy -> let r = recip (join (*) vx + join (*) vy) in (vy * r, negate vx * r)
-       instance RealFrac a => RealFrac ($t a) where
-        properFraction a = (w, a `withPrimal` pb) where
-             pa = primal a
-             (w, pb) = properFraction pa
-        truncate = discrete1 truncate
-        round    = discrete1 round
-        ceiling  = discrete1 ceiling
-        floor    = discrete1 floor
-       instance Erf a => Erf ($t a) where
-        erf = lift1 erf $ \x -> (fromInteger 2 / sqrt pi) * exp (negate x * x)
-        erfc = lift1 erfc $ \x -> (fromInteger (-2) / sqrt pi) * exp (negate x * x)
-        normcdf = lift1 normcdf $ \x -> (fromInteger (-1) / sqrt pi) * exp (x * x * fromRational (- recip 2) / sqrt (fromInteger 2))
-       instance InvErf a => InvErf ($t a) where
-        inverf = lift1 inverfc $ \x -> recip $ (fromInteger 2 / sqrt pi) * exp (negate x * x)
-        inverfc = lift1 inverfc $ \x -> recip $ negate (fromInteger 2 / sqrt pi) * exp (negate x * x)
-        invnormcdf = lift1 invnormcdf $ \x -> recip $ (fromInteger (-1) / sqrt pi) * exp (x * x * fromRational (- recip 2) / sqrt (fromInteger 2))
-       |]
+-- deriveNumeric :: ([Pred] -> [Pred]) -> Type -> Q [Dec]
+-- deriveNumeric f tCon = map fudgeCxt <$> lifted
+--     where
+--       t = pure tCon
+--       fudgeCxt (InstanceD cxt typ dec) = InstanceD (f cxt) typ dec
+--       fudgeCxt _ = error "Numeric.AD.Internal.Classes.deriveNumeric_fudgeCxt: Not InstanceD"
+--       lifted = [d|
+--        instance (Num a, Eq a) => Eq ($t a) where
+--         (==)          = discrete2 (==)
+--        instance (Num a, Ord a) => Ord ($t a) where
+--         compare       = discrete2 compare
+--        instance (Num a, Bounded a) => Bounded ($t a) where
+--         maxBound      = auto maxBound
+--         minBound      = auto minBound
+--        instance Num a => Num ($t a) where
+--         fromInteger 0  = zero
+--         fromInteger n = auto (fromInteger n)
+--         (+)          = (<+>) -- binary (+) one one
+--         (-)          = binary (-) (auto 1) (auto (-1)) -- TODO: <-> ? as it is, this might be pretty bad for Tower
+--         (*)          = lift2 (*) (\x y -> (y, x))
+--         negate       = lift1 negate (const (auto (-1)))
+--         abs          = lift1 abs signum
+--         signum a     = lift1 signum (const zero) a
+--        instance Fractional a => Fractional ($t a) where
+--         fromRational 0 = zero
+--         fromRational r = auto (fromRational r)
+--         x / y        = x * recip y
+--         recip        = lift1_ recip (const . negate . join (*))
+--        instance Floating a => Floating ($t a) where
+--         pi       = auto pi
+--         exp      = lift1_ exp const
+--         log      = lift1 log recip
+--         logBase x y = log y / log x
+--         sqrt     = lift1_ sqrt (\z _ -> recip (auto 2 * z))
+--         (**)     = (<**>)
+--         --x ** y
+--         --   | isKnownZero y     = 1
+--         --   | isKnownConstant y, y' <- primal y = lift1 (** y') ((y'*) . (**(y'-1))) x
+--         --   | otherwise         = lift2_ (**) (\z xi yi -> (yi * z / xi, z * log1 xi)) x y
+--         sin      = lift1 sin cos
+--         cos      = lift1 cos $ negate . sin
+--         tan      = lift1 tan $ recip . join (*) . cos
+--         asin     = lift1 asin $ \x -> recip (sqrt (auto 1 - join (*) x))
+--         acos     = lift1 acos $ \x -> negate (recip (sqrt (one - join (*) x)))
+--         atan     = lift1 atan $ \x -> recip (one + join (*) x)
+--         sinh     = lift1 sinh cosh
+--         cosh     = lift1 cosh sinh
+--         tanh     = lift1 tanh $ recip . join (*) . cosh
+--         asinh    = lift1 asinh $ \x -> recip (sqrt (one + join (*) x))
+--         acosh    = lift1 acosh $ \x -> recip (sqrt (join (*) x - one))
+--         atanh    = lift1 atanh $ \x -> recip (one - join (*) x)
+--        instance (Num a, Enum a) => Enum ($t a) where
+--         succ                 = lift1 succ (const one)
+--         pred                 = lift1 pred (const one)
+--         toEnum               = auto . toEnum
+--         fromEnum             = discrete1 fromEnum
+--         enumFrom a           = withPrimal a <$> enumFrom (primal a)
+--         enumFromTo a b       = withPrimal a <$> discrete2 enumFromTo a b
+--         enumFromThen a b     = zipWith (fromBy a delta) [0..] $ discrete2 enumFromThen a b where delta = b - a
+--         enumFromThenTo a b c = zipWith (fromBy a delta) [0..] $ discrete3 enumFromThenTo a b c where delta = b - a
+--        instance Real a => Real ($t a) where
+--         toRational      = discrete1 toRational
+--        instance RealFloat a => RealFloat ($t a) where
+--         floatRadix      = discrete1 floatRadix
+--         floatDigits     = discrete1 floatDigits
+--         floatRange      = discrete1 floatRange
+--         decodeFloat     = discrete1 decodeFloat
+--         encodeFloat m e = auto (encodeFloat m e)
+--         isNaN           = discrete1 isNaN
+--         isInfinite      = discrete1 isInfinite
+--         isDenormalized  = discrete1 isDenormalized
+--         isNegativeZero  = discrete1 isNegativeZero
+--         isIEEE          = discrete1 isIEEE
+--         exponent = exponent
+--         scaleFloat n = unary (scaleFloat n) (scaleFloat n one)
+--         significand x =  unary significand (scaleFloat (- floatDigits x) one) x
+--         atan2 = lift2 atan2 $ \vx vy -> let r = recip (join (*) vx + join (*) vy) in (vy * r, negate vx * r)
+--        instance RealFrac a => RealFrac ($t a) where
+--         properFraction a = (w, a `withPrimal` pb) where
+--              pa = primal a
+--              (w, pb) = properFraction pa
+--         truncate = discrete1 truncate
+--         round    = discrete1 round
+--         ceiling  = discrete1 ceiling
+--         floor    = discrete1 floor
+--        instance Erf a => Erf ($t a) where
+--         erf = lift1 erf $ \x -> (fromInteger 2 / sqrt pi) * exp (negate x * x)
+--         erfc = lift1 erfc $ \x -> (fromInteger (-2) / sqrt pi) * exp (negate x * x)
+--         normcdf = lift1 normcdf $ \x -> (fromInteger (-1) / sqrt pi) * exp (x * x * fromRational (- recip 2) / sqrt (fromInteger 2))
+--        instance InvErf a => InvErf ($t a) where
+--         inverf = lift1 inverfc $ \x -> recip $ (fromInteger 2 / sqrt pi) * exp (negate x * x)
+--         inverfc = lift1 inverfc $ \x -> recip $ negate (fromInteger 2 / sqrt pi) * exp (negate x * x)
+--         invnormcdf = lift1 invnormcdf $ \x -> recip $ (fromInteger (-1) / sqrt pi) * exp (x * x * fromRational (- recip 2) / sqrt (fromInteger 2))
+--        |]
 
 deriveNumeric' :: ([Pred] -> [Pred]) -> Type -> Type -> Q [Dec]
 deriveNumeric' f tCon s' = map fudgeCxt <$> lifted

47d201fd70bb3a06e7b2fd172d58607163cdc9ba
Edward Kmett <ekmett@gmail.com>
2021-02-14 15:48:02 -0800

    drop more code duplication in ForwardDouble

--- src/Numeric/AD/Internal/Forward/Double.hs
+++ src/Numeric/AD/Internal/Forward/Double.hs
@@ -36,7 +36,6 @@ module Numeric.AD.Internal.Forward.Double
 import Data.Foldable (toList)
 import Data.Traversable (mapAccumL)
 import Control.Monad (join)
-import Data.Function (on)
 import Data.Number.Erf
 import Numeric.AD.Internal.Combinators
 import Numeric.AD.Internal.Identity
@@ -105,98 +104,11 @@ instance Jacobian ForwardDouble where
     (Id dadb, Id dadc) = df (Id a) (Id b) (Id c)
     da = dadb * db + dc * dadc
 
-instance Eq ForwardDouble where
-  (==)          = on (==) primal
-
-instance Ord ForwardDouble where
-  compare       = on compare primal
-
-instance Num ForwardDouble where
-  fromInteger 0  = zero
-  fromInteger n = auto (fromInteger n)
-  (+)          = (<+>) -- binary (+) 1 1
-  (-)          = binary (-) (auto 1) (auto (-1)) -- TODO: <-> ? as it is, this might be pretty bad for Tower
-  (*)          = lift2 (*) (\x y -> (y, x))
-  negate       = lift1 negate (const (auto (-1)))
-  abs          = lift1 abs signum
-  signum a     = lift1 signum (const zero) a
-
-instance Fractional ForwardDouble where
-  fromRational 0 = zero
-  fromRational r = auto (fromRational r)
-  x / y        = x * recip y
-  recip        = lift1_ recip (const . negate . join (*))
-
-instance Floating ForwardDouble where
-  pi       = auto pi
-  exp      = lift1_ exp const
-  log      = lift1 log recip
-  logBase x y = log y / log x
-  sqrt     = lift1_ sqrt (\z _ -> recip (auto 2 * z))
-  ForwardDouble 0 0 ** ForwardDouble a _ = ForwardDouble (0 ** a) 0
-  _ ** ForwardDouble 0 0                 = ForwardDouble 1 0
-  x ** ForwardDouble y 0 = lift1 (**y) (\z -> y *^ z ** Id (y - 1)) x
-  x ** y                 = lift2_ (**) (\z xi yi -> (yi * z / xi, z * log xi)) x y
-  sin      = lift1 sin cos
-  cos      = lift1 cos $ negate . sin
-  tan      = lift1 tan $ recip . join (*) . cos
-  asin     = lift1 asin $ \x -> recip (sqrt (auto 1 - join (*) x))
-  acos     = lift1 acos $ \x -> negate (recip (sqrt (1 - join (*) x)))
-  atan     = lift1 atan $ \x -> recip (1 + join (*) x)
-  sinh     = lift1 sinh cosh
-  cosh     = lift1 cosh sinh
-  tanh     = lift1 tanh $ recip . join (*) . cosh
-  asinh    = lift1 asinh $ \x -> recip (sqrt (1 + join (*) x))
-  acosh    = lift1 acosh $ \x -> recip (sqrt (join (*) x - 1))
-  atanh    = lift1 atanh $ \x -> recip (1 - join (*) x)
-
-instance Enum ForwardDouble where
-  succ                 = lift1 succ (const 1)
-  pred                 = lift1 pred (const 1)
-  toEnum               = auto . toEnum
-  fromEnum             = fromEnum . primal
-  enumFrom a           = withPrimal a <$> enumFrom (primal a)
-  enumFromTo a b       = withPrimal a <$> enumFromTo (primal a) (primal b)
-  enumFromThen a b     = zipWith (fromBy a delta) [0..] $ enumFromThen (primal a) (primal b) where delta = b - a
-  enumFromThenTo a b c = zipWith (fromBy a delta) [0..] $ enumFromThenTo (primal a) (primal b) (primal c) where delta = b - a
-
-instance Real ForwardDouble where
-  toRational      = toRational . primal
-
-instance RealFloat ForwardDouble where
-  floatRadix      = floatRadix . primal
-  floatDigits     = floatDigits . primal
-  floatRange      = floatRange . primal
-  decodeFloat     = decodeFloat . primal
-  encodeFloat m e = auto (encodeFloat m e)
-  isNaN           = isNaN . primal
-  isInfinite      = isInfinite . primal
-  isDenormalized  = isDenormalized . primal
-  isNegativeZero  = isNegativeZero . primal
-  isIEEE          = isIEEE . primal
-  exponent = exponent
-  scaleFloat n = unary (scaleFloat n) (scaleFloat n 1)
-  significand x =  unary significand (scaleFloat (- floatDigits x) 1) x
-  atan2 = lift2 atan2 $ \vx vy -> let r = recip (join (*) vx + join (*) vy) in (vy * r, negate vx * r)
-
-instance RealFrac ForwardDouble where
-  properFraction a = (w, a `withPrimal` pb) where
-    pa = primal a
-    (w, pb) = properFraction pa
-  truncate = truncate . primal
-  round    = round . primal
-  ceiling  = ceiling . primal
-  floor    = floor . primal
-
-instance Erf ForwardDouble where
-  erf = lift1 erf $ \x -> (2 / sqrt pi) * exp (negate x * x)
-  erfc = lift1 erfc $ \x -> ((-2) / sqrt pi) * exp (negate x * x)
-  normcdf = lift1 normcdf $ \x -> recip (sqrt (2 * pi)) * exp (- x * x / 2)
-
-instance InvErf ForwardDouble where
-  inverf = lift1_ inverf $ \x _ -> sqrt pi / 2 * exp (x * x)
-  inverfc = lift1_ inverfc $ \x _ -> negate (sqrt pi / 2) * exp (x * x)
-  invnormcdf = lift1_ invnormcdf $ \x _ -> sqrt (2 * pi) * exp (x * x / 2)
+#define HEAD ForwardDouble
+#define BODY1(x)
+#define BODY2(x,y)
+#define NO_Bounded
+#include "instances.h"
 
 bind :: Traversable f => (f ForwardDouble -> b) -> f Double -> f b
 bind f as = snd $ mapAccumL outer (0 :: Int) as where
@@ -226,3 +138,6 @@ transposeWith :: (Functor f, Foldable f, Traversable g) => (b -> f a -> c) -> f 
 transposeWith f as = snd . mapAccumL go xss0 where
   go xss b = (tail <$> xss, f b (head <$> xss))
   xss0 = toList <$> as
+
+mul :: ForwardDouble -> ForwardDouble -> ForwardDouble
+mul = lift2 (*) (\x y -> (y, x))

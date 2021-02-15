f09cbc4ff40c468ec8e7149cebeb151933fae77d
Edward Kmett <ekmett@gmail.com>
2021-02-14 18:11:36 -0800

    add Numeric.AD.Mode.Sparse.Double

---
+++ src/Numeric/AD/Mode/Sparse/Double.hs
@@ -0,0 +1,162 @@
+{-# LANGUAGE Rank2Types #-}
+-----------------------------------------------------------------------------
+-- |
+-- Copyright   : (c) Edward Kmett 2010-2021
+-- License     : BSD3
+-- Maintainer  : ekmett@gmail.com
+-- Stability   : experimental
+-- Portability : GHC only
+--
+-- Higher order derivatives via a \"dual number tower\".
+--
+-----------------------------------------------------------------------------
+
+module Numeric.AD.Mode.Sparse.Double
+  ( AD, SparseDouble, auto
+  -- * Sparse Gradients
+  , grad
+  , grad'
+  , grads
+  , gradWith
+  , gradWith'
+
+  -- * Sparse Jacobians (synonyms)
+  , jacobian
+  , jacobian'
+  , jacobianWith
+  , jacobianWith'
+  , jacobians
+
+  -- * Sparse Hessians
+  , hessian
+  , hessian'
+
+  , hessianF
+  , hessianF'
+  ) where
+
+import Control.Comonad.Cofree (Cofree)
+import Numeric.AD.Internal.Sparse.Double (SparseDouble)
+import qualified Numeric.AD.Rank1.Sparse.Double as Rank1
+import Numeric.AD.Internal.Type
+import Numeric.AD.Mode
+
+-- | The 'grad' function calculates the gradient of a non-scalar-to-scalar function with sparse-mode AD in a single pass.
+--
+--
+-- >>> grad (\[x,y,z] -> x*y+z) [1,2,3]
+-- [2,1,1]
+--
+-- >>> grad (\[x,y] -> x**y) [0,2]
+-- [0.0,NaN]
+grad
+  :: Traversable f
+  => (forall s. f (AD s SparseDouble) -> AD s SparseDouble)
+  -> f Double
+  -> f Double
+grad f = Rank1.grad (runAD.f.fmap AD)
+{-# INLINE grad #-}
+
+grad'
+  :: Traversable f
+  => (forall s. f (AD s SparseDouble) -> AD s SparseDouble)
+  -> f Double
+  -> (Double, f Double)
+grad' f = Rank1.grad' (runAD.f.fmap AD)
+{-# INLINE grad' #-}
+
+gradWith
+  :: Traversable f
+  => (Double -> Double -> b)
+  -> (forall s. f (AD s SparseDouble) -> AD s SparseDouble)
+  -> f Double
+  -> f b
+gradWith g f = Rank1.gradWith g (runAD.f.fmap AD)
+{-# INLINE gradWith #-}
+
+gradWith'
+  :: Traversable f
+  => (Double -> Double -> b)
+  -> (forall s. f (AD s SparseDouble) -> AD s SparseDouble)
+  -> f Double
+  -> (Double, f b)
+gradWith' g f = Rank1.gradWith' g (runAD.f.fmap AD)
+{-# INLINE gradWith' #-}
+
+jacobian
+  :: (Traversable f, Functor g)
+  => (forall s. f (AD s SparseDouble) -> g (AD s SparseDouble))
+  -> f Double -> g (f Double)
+jacobian f = Rank1.jacobian (fmap runAD.f.fmap AD)
+{-# INLINE jacobian #-}
+
+jacobian'
+  :: (Traversable f, Functor g)
+  => (forall s. f (AD s SparseDouble) -> g (AD s SparseDouble))
+  -> f Double
+  -> g (Double, f Double)
+jacobian' f = Rank1.jacobian' (fmap runAD.f.fmap AD)
+{-# INLINE jacobian' #-}
+
+jacobianWith
+  :: (Traversable f, Functor g)
+  => (Double -> Double -> b)
+  -> (forall s. f (AD s SparseDouble) -> g (AD s SparseDouble))
+  -> f Double
+  -> g (f b)
+jacobianWith g f = Rank1.jacobianWith g (fmap runAD.f.fmap AD)
+{-# INLINE jacobianWith #-}
+
+jacobianWith'
+  :: (Traversable f, Functor g)
+  => (Double -> Double -> b)
+  -> (forall s. f (AD s SparseDouble) -> g (AD s SparseDouble))
+  -> f Double
+  -> g (Double, f b)
+jacobianWith' g f = Rank1.jacobianWith' g (fmap runAD.f.fmap AD)
+{-# INLINE jacobianWith' #-}
+
+grads
+  :: Traversable f
+  => (forall s. f (AD s SparseDouble) -> AD s SparseDouble)
+  -> f Double -> Cofree f Double
+grads f = Rank1.grads (runAD.f.fmap AD)
+{-# INLINE grads #-}
+
+jacobians
+  :: (Traversable f, Functor g)
+  => (forall s. f (AD s SparseDouble) -> g (AD s SparseDouble))
+  -> f Double
+  -> g (Cofree f Double)
+jacobians f = Rank1.jacobians (fmap runAD.f.fmap AD)
+{-# INLINE jacobians #-}
+
+hessian
+  :: Traversable f
+  => (forall s. f (AD s SparseDouble) -> AD s SparseDouble)
+  -> f Double
+  -> f (f Double)
+hessian f = Rank1.hessian (runAD.f.fmap AD)
+{-# INLINE hessian #-}
+
+hessian'
+  :: Traversable f
+  => (forall s. f (AD s SparseDouble) -> AD s SparseDouble)
+  -> f Double -> (Double, f (Double, f Double))
+hessian' f = Rank1.hessian' (runAD.f.fmap AD)
+{-# INLINE hessian' #-}
+
+hessianF
+  :: (Traversable f, Functor g)
+  => (forall s. f (AD s SparseDouble) -> g (AD s SparseDouble))
+  -> f Double -> g (f (f Double))
+hessianF f = Rank1.hessianF (fmap runAD.f.fmap AD)
+{-# INLINE hessianF #-}
+
+hessianF'
+  :: (Traversable f, Functor g)
+  => (forall s. f (AD s SparseDouble) -> g (AD s SparseDouble))
+  -> f Double
+  -> g (Double, f (Double, f Double))
+hessianF' f = Rank1.hessianF' (fmap runAD.f.fmap AD)
+{-# INLINE hessianF' #-}
---
+++ src/Numeric/AD/Rank1/Sparse/Double.hs
@@ -0,0 +1,192 @@
+-----------------------------------------------------------------------------
+-- |
+-- Copyright   : (c) Edward Kmett 2010-2021
+-- License     : BSD3
+-- Maintainer  : ekmett@gmail.com
+-- Stability   : experimental
+-- Portability : GHC only
+--
+-- Higher order derivatives via a \"dual number tower\".
+--
+-----------------------------------------------------------------------------
+
+module Numeric.AD.Rank1.Sparse.Double
+  ( SparseDouble
+  , auto
+  -- * Sparse Gradients
+  , grad
+  , grad'
+  , gradWith
+  , gradWith'
+  -- * Variadic Gradients
+  -- $vgrad
+  , Grad
+  , vgrad
+  -- * Higher-Order Gradients
+  , grads
+  -- * Variadic Higher-Order Gradients
+  , Grads
+  , vgrads
+
+  -- * Sparse Jacobians (synonyms)
+  , jacobian
+  , jacobian'
+  , jacobianWith
+  , jacobianWith'
+  , jacobians
+
+  -- * Sparse Hessians
+  , hessian
+  , hessian'
+
+  , hessianF
+  , hessianF'
+
+  ) where
+
+import Control.Comonad
+import Control.Comonad.Cofree
+import Numeric.AD.Jet
+import Numeric.AD.Internal.Sparse.Double
+import Numeric.AD.Internal.Combinators
+import Numeric.AD.Mode
+
+second :: (a -> b) -> (c, a) -> (c, b)
+second g (a,b) = (a, g b)
+{-# INLINE second #-}
+
+grad
+  :: Traversable f
+  => (f SparseDouble -> SparseDouble)
+  -> f Double -> f Double
+grad f as = d as $ apply f as
+{-# INLINE grad #-}
+
+grad'
+  :: Traversable f
+  => (f SparseDouble -> SparseDouble)
+  -> f Double -> (Double, f Double)
+grad' f as = d' as $ apply f as
+{-# INLINE grad' #-}
+
+gradWith
+  :: Traversable f
+  => (Double -> Double -> b)
+  -> (f SparseDouble -> SparseDouble)
+  -> f Double
+  -> f b
+gradWith g f as = zipWithT g as $ grad f as
+{-# INLINE gradWith #-}
+
+gradWith'
+  :: Traversable f
+  => (Double -> Double -> b)
+  -> (f SparseDouble -> SparseDouble)
+  -> f Double
+  -> (Double, f b)
+gradWith' g f as = second (zipWithT g as) $ grad' f as
+{-# INLINE gradWith' #-}
+
+jacobian
+  :: (Traversable f, Functor g)
+  => (f SparseDouble -> g SparseDouble)
+  -> f Double
+  -> g (f Double)
+jacobian f as = d as <$> apply f as
+{-# INLINE jacobian #-}
+
+jacobian'
+  :: (Traversable f, Functor g)
+  => (f SparseDouble -> g SparseDouble)
+  -> f Double
+  -> g (Double, f Double)
+jacobian' f as = d' as <$> apply f as
+{-# INLINE jacobian' #-}
+
+jacobianWith
+  :: (Traversable f, Functor g)
+  => (Double -> Double -> b)
+  -> (f SparseDouble -> g SparseDouble)
+  -> f Double
+  -> g (f b)
+jacobianWith g f as = zipWithT g as <$> jacobian f as
+{-# INLINE jacobianWith #-}
+
+jacobianWith'
+  :: (Traversable f, Functor g)
+  => (Double -> Double -> b)
+  -> (f SparseDouble -> g SparseDouble)
+  -> f Double
+  -> g (Double, f b)
+jacobianWith' g f as = second (zipWithT g as) <$> jacobian' f as
+{-# INLINE jacobianWith' #-}
+
+grads
+  :: Traversable f
+  => (f SparseDouble -> SparseDouble)
+  -> f Double
+  -> Cofree f Double
+grads f as = ds as $ apply f as
+{-# INLINE grads #-}
+
+jacobians
+  :: (Traversable f, Functor g)
+  => (f SparseDouble -> g SparseDouble)
+  -> f Double
+  -> g (Cofree f Double)
+jacobians f as = ds as <$> apply f as
+{-# INLINE jacobians #-}
+
+d2 :: Functor f
+  => Cofree f a
+  -> f (f a)
+d2 = headJet . tailJet . tailJet . jet
+{-# INLINE d2 #-}
+
+d2'
+  :: Functor f
+  => Cofree f a
+  -> (a, f (a, f a))
+d2' (a :< as) = (a, fmap (\(da :< das) -> (da, extract <$> das)) as)
+{-# INLINE d2' #-}
+
+hessian
+  :: Traversable f
+  => (f SparseDouble -> SparseDouble)
+  -> f Double
+  -> f (f Double)
+hessian f as = d2 $ grads f as
+{-# INLINE hessian #-}
+
+hessian'
+  :: Traversable f
+  => (f SparseDouble -> SparseDouble)
+  -> f Double
+  -> (Double, f (Double, f Double))
+hessian' f as = d2' $ grads f as
+{-# INLINE hessian' #-}
+
+hessianF
+  :: (Traversable f, Functor g)
+  => (f SparseDouble -> g SparseDouble)
+  -> f Double
+  -> g (f (f Double))
+hessianF f as = d2 <$> jacobians f as
+{-# INLINE hessianF #-}
+
+hessianF'
+  :: (Traversable f, Functor g)
+  => (f SparseDouble -> g SparseDouble)
+  -> f Double
+  -> g (Double, f (Double, f Double))
+hessianF' f as = d2' <$> jacobians f as
+{-# INLINE hessianF' #-}
+
+-- $vgrad
+--
+-- Variadic combinators for variadic mixed-mode automatic differentiation.
+--
+-- Unfortunately, variadicity comes at the expense of being able to use
+-- quantification to avoid sensitivity confusion, so be careful when
+-- counting the number of 'auto' calls you use when taking the gradient
+-- of a function that takes gradients!

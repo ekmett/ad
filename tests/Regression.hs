6827864e60efa7acccaecfbb528339178d9f4c44
Julian Brunner <julian.brunner@gmail.com>
2024-03-08 07:24:03 +0100

    use higher-order combinators for comparison

--- tests/Regression.hs
+++ tests/Regression.hs
@@ -29,22 +29,22 @@ mode name diff grad jacobian hessian = testGroup name [basic diff grad jacobian 
 basic :: Diff -> Grad -> Jacobian -> Hessian -> TestTree
 basic diff grad jacobian hessian = testGroup "basic" [tdiff, tgrad, tjacobian, thessian] where
   tdiff = testCase "diff" $ do
-    assertNearList [11, 5.5, 3, 3.5, 7, 13.5, 23, 35.5, 51] $ diff p <$> [-2, -1.5, -1, -0.5, 0, 0.5, 1, 1.5, 2]
-    assertNearList [nan, inf, 1, 0.5, 0.25] $ diff sqrt <$> [-1, 0, 0.25, 1, 4]
-    assertNearList [1, 0, 1] $ [diff sin, diff cos, diff tan] <*> [0]
-    assertNearList [-1, 0, 1] $ diff abs <$> [-1, 0, 1]
-    assertNearList [1, exp 1, inf, 1] $ [diff exp, diff log] <*> [0, 1]
+    expect (list eq) [11, 5.5, 3, 3.5, 7, 13.5, 23, 35.5, 51] $ diff p <$> [-2, -1.5, -1, -0.5, 0, 0.5, 1, 1.5, 2]
+    expect (list eq) [nan, inf, 1, 0.5, 0.25] $ diff sqrt <$> [-1, 0, 0.25, 1, 4]
+    expect (list eq) [1, 0, 1] $ [diff sin, diff cos, diff tan] <*> [0]
+    expect (list eq) [-1, 0, 1] $ diff abs <$> [-1, 0, 1]
+    expect (list eq) [1, exp 1, inf, 1] $ [diff exp, diff log] <*> [0, 1]
   tgrad = testCase "grad" $ do
-    assertNearList [2, 1, 1] $ grad f [1, 2, 3]
-    assertNearList [1, 0.25] $ grad h [2, 8]
-    assertNearList [0, nan] $ grad power [0, 2]
+    expect (list eq) [2, 1, 1] $ grad f [1, 2, 3]
+    expect (list eq) [1, 0.25] $ grad h [2, 8]
+    expect (list eq) [0, nan] $ grad power [0, 2]
   tjacobian = testCase "jacobian" $ do
-    assertNearMatrix [[0, 1], [1, 0], [1, 2]] $ jacobian g [2, 1]
+    expect (list $ list eq) [[0, 1], [1, 0], [1, 2]] $ jacobian g [2, 1]
   thessian = testCase "hessian" $ do
-    assertNearMatrix [[0, 1, 0], [1, 0, 0], [0, 0, 0]] $ hessian f [1, 2, 3]
-    assertNearMatrix [[0, 0], [0, 0]] $ hessian sum [1, 2]
-    assertNearMatrix [[0, 1], [1, 0]] $ hessian product [1, 2]
-    assertNearMatrix [[2, 1], [1, 0]] $ hessian power [1, 2]
+    expect (list $ list eq) [[0, 1, 0], [1, 0, 0], [0, 0, 0]] $ hessian f [1, 2, 3]
+    expect (list $ list eq) [[0, 0], [0, 0]] $ hessian sum [1, 2]
+    expect (list $ list eq) [[0, 1], [1, 0]] $ hessian product [1, 2]
+    expect (list $ list eq) [[2, 1], [1, 0]] $ hessian power [1, 2]
   sum = \ [x, y] -> x + y
   product = \ [x, y] -> x * y
   power = \ [x, y] -> x ** y
@@ -56,56 +56,44 @@ basic diff grad jacobian hessian = testGroup "basic" [tdiff, tgrad, tjacobian, t
 -- Reverse.Double +ffi initializes the tape with a block of size 4096
 -- The large term in this function forces the allocation of an additional block
 issue97 :: Diff -> TestTree
-issue97 diff = testCase "issue-97" $ assertNear 5000 $ diff f 0 where f = sum . replicate 5000
+issue97 diff = testCase "issue-97" $ expect eq 5000 $ diff f 0 where f = sum . replicate 5000
 
 issue104 :: Diff -> Grad -> TestTree
 issue104 diff grad = testGroup "issue-104" [inside, outside] where
   inside = testGroup "inside" [tdiff, tgrad] where
     tdiff = testCase "diff" $ do
-      assertNearList [nan, nan] $ diff (0 `f`) <$> [0, 1]
-      assertNearList [inf, 0.5] $ diff (1 `f`) <$> [0, 1]
-      assertNearList [nan, nan] $ diff (`f` 0) <$> [0, 1]
-      assertNearList [inf, 0.5] $ diff (`f` 1) <$> [0, 1]
+      expect (list eq) [nan, nan] $ diff (0 `f`) <$> [0, 1]
+      expect (list eq) [inf, 0.5] $ diff (1 `f`) <$> [0, 1]
+      expect (list eq) [nan, nan] $ diff (`f` 0) <$> [0, 1]
+      expect (list eq) [inf, 0.5] $ diff (`f` 1) <$> [0, 1]
     tgrad = testCase "grad" $ do
-      assertNearList [nan, nan] $ grad (binary f) [0, 0]
-      assertNearList [nan, inf] $ grad (binary f) [1, 0]
-      assertNearList [inf, nan] $ grad (binary f) [0, 1]
-      assertNearList [0.5, 0.5] $ grad (binary f) [1, 1]
+      expect (list eq) [nan, nan] $ grad (binary f) [0, 0]
+      expect (list eq) [nan, inf] $ grad (binary f) [1, 0]
+      expect (list eq) [inf, nan] $ grad (binary f) [0, 1]
+      expect (list eq) [0.5, 0.5] $ grad (binary f) [1, 1]
     f x y = sqrt $ x * y -- grad f [x, y] = [y / (2 * f x y), x / (2 * f x y)]
   outside = testGroup "outside" [tdiff, tgrad] where
     tdiff = testCase "diff" $ do
-      assertNearList [nan, 0.0] $ diff (0 `f`) <$> [0, 1]
-      assertNearList [inf, 0.5] $ diff (1 `f`) <$> [0, 1]
-      assertNearList [nan, 0.0] $ diff (`f` 0) <$> [0, 1]
-      assertNearList [inf, 0.5] $ diff (`f` 1) <$> [0, 1]
+      expect (list eq) [nan, 0.0] $ diff (0 `f`) <$> [0, 1]
+      expect (list eq) [inf, 0.5] $ diff (1 `f`) <$> [0, 1]
+      expect (list eq) [nan, 0.0] $ diff (`f` 0) <$> [0, 1]
+      expect (list eq) [inf, 0.5] $ diff (`f` 1) <$> [0, 1]
     tgrad = testCase "grad" $ do
-      assertNearList [nan, nan] $ grad (binary f) [0, 0]
-      assertNearList [0.0, inf] $ grad (binary f) [1, 0]
-      assertNearList [inf, 0.0] $ grad (binary f) [0, 1]
-      assertNearList [0.5, 0.5] $ grad (binary f) [1, 1]
+      expect (list eq) [nan, nan] $ grad (binary f) [0, 0]
+      expect (list eq) [0.0, inf] $ grad (binary f) [1, 0]
+      expect (list eq) [inf, 0.0] $ grad (binary f) [0, 1]
+      expect (list eq) [0.5, 0.5] $ grad (binary f) [1, 1]
     f x y = sqrt x * sqrt y -- grad f [x, y] = [sqrt y / 2 sqrt x, sqrt x / 2 sqrt y]
   binary f = \ [x, y] -> f x y
 
-near :: Double -> Double -> Bool
-near a b = isNaN a && isNaN b || a == b
+eq :: Double -> Double -> Bool
+eq a b = isNaN a && isNaN b || a == b
 
-nearList :: [Double] -> [Double] -> Bool
-nearList as bs = length as == length bs && and (zipWith near as bs)
+list :: (a -> a -> Bool) -> [a] -> [a] -> Bool
+list eq as bs = length as == length bs && and (zipWith eq as bs)
 
-nearMatrix :: [[Double]] -> [[Double]] -> Bool
-nearMatrix as bs = length as == length bs && and (zipWith nearList as bs)
-
-assertNear :: Double -> Double -> Assertion
-assertNear a b = near a b @? expect a b
-
-assertNearList :: [Double] -> [Double] -> Assertion
-assertNearList a b = nearList a b @? expect a b
-
-assertNearMatrix :: [[Double]] -> [[Double]] -> Assertion
-assertNearMatrix a b = nearMatrix a b @? expect a b
-
-expect :: Show a => a -> a -> String
-expect a b = printf "expected %s but got %s" (show a) (show b)
+expect :: Show a => (a -> a -> Bool) -> a -> a -> Assertion
+expect eq a b = eq a b @? printf "expected %s but got %s" (show a) (show b)
 
 nan :: Double
 nan = 0 / 0

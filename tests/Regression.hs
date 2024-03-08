f842a14a8ac0697fea80015cf8c7d848363fc52a
Julian Brunner <julian.brunner@gmail.com>
2024-03-08 07:35:37 +0100

    supply diff' to tests

--- tests/Regression.hs
+++ tests/Regression.hs
@@ -10,7 +10,7 @@ import Text.Printf
 import Test.Tasty
 import Test.Tasty.HUnit
 
-type Diff = (forall a. Floating a => a -> a) -> Double -> Double
+type Diff = (forall a. Floating a => a -> a) -> Double -> (Double, Double)
 type Grad = (forall a. Floating a => [a] -> a) -> [Double] -> [Double]
 type Jacobian = (forall a. Floating a => [a] -> [a]) -> [Double] -> [[Double]]
 type Hessian = (forall a. Floating a => [a] -> a) -> [Double] -> [[Double]]
@@ -20,8 +20,8 @@ main = defaultMain tests
 
 tests :: TestTree
 tests = testGroup "tests" [
-  mode "reverse" (\ f -> R.diff f) (\ f -> R.grad f) (\ f -> R.jacobian f) (\ f -> R.hessian f),
-  mode "reverse-double" (\ f -> RD.diff f) (\ f -> RD.grad f) (\ f -> RD.jacobian f) (\ f -> RD.hessian f)]
+  mode "reverse" (\ f -> R.diff' f) (\ f -> R.grad f) (\ f -> R.jacobian f) (\ f -> R.hessian f),
+  mode "reverse-double" (\ f -> RD.diff' f) (\ f -> RD.grad f) (\ f -> RD.jacobian f) (\ f -> RD.hessian f)]
 
 mode :: String -> Diff -> Grad -> Jacobian -> Hessian -> TestTree
 mode name diff grad jacobian hessian = testGroup name [basic diff grad jacobian hessian, issue97 diff, issue104 diff grad]
@@ -29,11 +29,11 @@ mode name diff grad jacobian hessian = testGroup name [basic diff grad jacobian 
 basic :: Diff -> Grad -> Jacobian -> Hessian -> TestTree
 basic diff grad jacobian hessian = testGroup "basic" [tdiff, tgrad, tjacobian, thessian] where
   tdiff = testCase "diff" $ do
-    expect (list eq) [11, 5.5, 3, 3.5, 7, 13.5, 23, 35.5, 51] $ diff p <$> [-2, -1.5, -1, -0.5, 0, 0.5, 1, 1.5, 2]
-    expect (list eq) [nan, inf, 1, 0.5, 0.25] $ diff sqrt <$> [-1, 0, 0.25, 1, 4]
-    expect (list eq) [1, 0, 1] $ [diff sin, diff cos, diff tan] <*> [0]
-    expect (list eq) [-1, 0, 1] $ diff abs <$> [-1, 0, 1]
-    expect (list eq) [1, exp 1, inf, 1] $ [diff exp, diff log] <*> [0, 1]
+    expect (list eq) [11, 5.5, 3, 3.5, 7, 13.5, 23, 35.5, 51] $ snd . diff p <$> [-2, -1.5, -1, -0.5, 0, 0.5, 1, 1.5, 2]
+    expect (list eq) [nan, inf, 1, 0.5, 0.25] $ snd . diff sqrt <$> [-1, 0, 0.25, 1, 4]
+    expect (list eq) [1, 0, 1] $ [snd . diff sin, snd . diff cos, snd . diff tan] <*> [0]
+    expect (list eq) [-1, 0, 1] $ snd . diff abs <$> [-1, 0, 1]
+    expect (list eq) [1, exp 1, inf, 1] $ [snd . diff exp, snd . diff log] <*> [0, 1]
   tgrad = testCase "grad" $ do
     expect (list eq) [2, 1, 1] $ grad f [1, 2, 3]
     expect (list eq) [1, 0.25] $ grad h [2, 8]
@@ -56,16 +56,16 @@ basic diff grad jacobian hessian = testGroup "basic" [tdiff, tgrad, tjacobian, t
 -- Reverse.Double +ffi initializes the tape with a block of size 4096
 -- The large term in this function forces the allocation of an additional block
 issue97 :: Diff -> TestTree
-issue97 diff = testCase "issue-97" $ expect eq 5000 $ diff f 0 where f = sum . replicate 5000
+issue97 diff = testCase "issue-97" $ expect eq 5000 $ snd $ diff f 0 where f = sum . replicate 5000
 
 issue104 :: Diff -> Grad -> TestTree
 issue104 diff grad = testGroup "issue-104" [inside, outside] where
   inside = testGroup "inside" [tdiff, tgrad] where
     tdiff = testCase "diff" $ do
-      expect (list eq) [nan, nan] $ diff (0 `f`) <$> [0, 1]
-      expect (list eq) [inf, 0.5] $ diff (1 `f`) <$> [0, 1]
-      expect (list eq) [nan, nan] $ diff (`f` 0) <$> [0, 1]
-      expect (list eq) [inf, 0.5] $ diff (`f` 1) <$> [0, 1]
+      expect (list eq) [nan, nan] $ snd . diff (0 `f`) <$> [0, 1]
+      expect (list eq) [inf, 0.5] $ snd . diff (1 `f`) <$> [0, 1]
+      expect (list eq) [nan, nan] $ snd . diff (`f` 0) <$> [0, 1]
+      expect (list eq) [inf, 0.5] $ snd . diff (`f` 1) <$> [0, 1]
     tgrad = testCase "grad" $ do
       expect (list eq) [nan, nan] $ grad (binary f) [0, 0]
       expect (list eq) [nan, inf] $ grad (binary f) [1, 0]
@@ -74,10 +74,10 @@ issue104 diff grad = testGroup "issue-104" [inside, outside] where
     f x y = sqrt $ x * y -- grad f [x, y] = [y / (2 * f x y), x / (2 * f x y)]
   outside = testGroup "outside" [tdiff, tgrad] where
     tdiff = testCase "diff" $ do
-      expect (list eq) [nan, 0.0] $ diff (0 `f`) <$> [0, 1]
-      expect (list eq) [inf, 0.5] $ diff (1 `f`) <$> [0, 1]
-      expect (list eq) [nan, 0.0] $ diff (`f` 0) <$> [0, 1]
-      expect (list eq) [inf, 0.5] $ diff (`f` 1) <$> [0, 1]
+      expect (list eq) [nan, 0.0] $ snd . diff (0 `f`) <$> [0, 1]
+      expect (list eq) [inf, 0.5] $ snd . diff (1 `f`) <$> [0, 1]
+      expect (list eq) [nan, 0.0] $ snd . diff (`f` 0) <$> [0, 1]
+      expect (list eq) [inf, 0.5] $ snd . diff (`f` 1) <$> [0, 1]
     tgrad = testCase "grad" $ do
       expect (list eq) [nan, nan] $ grad (binary f) [0, 0]
       expect (list eq) [0.0, inf] $ grad (binary f) [1, 0]
